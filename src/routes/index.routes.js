c6e68cf50ba67d486bbfe248d0421b173a1d000e
Thomas K <thmskffr@gmail.com>
2022-06-24 12:06:45 +0200

    basic GET routes working

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const albumsRouter = require("./albums.routes");
 const tracksRouter = require("./tracks.routes");
 
-
+router.use("/albums", albumsRouter);
+router.use("/tracks", tracksRouter);
 
 module.exports = router;
--- src/routes/tracks.routes.js
+++ src/routes/tracks.routes.js
@@ -1,4 +1,30 @@
 const router = require("express").Router();
 const connection = require("../db-config");
 
+router.get("/", (req, res) => {
+    connection.query("SELECT * FROM track",
+    (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error while retrieving from database")
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    connection.query("SELECT * FROM track WHERE id=?",
+    [id],
+    (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error while retrieving from database");
+        } else {
+            res.json(result);
+        }
+    });
+});
+
 module.exports = router;
