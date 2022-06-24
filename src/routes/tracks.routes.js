8982084e3ef6195d6f91977b227f0f6eb5bdab77
Thomas K <thmskffr@gmail.com>
2022-06-24 12:13:28 +0200

    GET route tracks by album OK

--- src/routes/tracks.routes.js
+++ src/routes/tracks.routes.js
@@ -27,4 +27,18 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.get("/albumtracks/:id", (req, res) => {
+    const id = req.params.id;
+    connection.query("SELECT * FROM track WHERE id_album=?",
+    [id],
+    (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error while retrieving from database");
+        } else {
+            res.json(result);
+        }
+    });
+});
+
 module.exports = router;
